de48f53e3f49ce0515b3a6d941d9dcc29078bae1
Hangil <105782314+hangil2010@users.noreply.github.com>
2025-03-20 17:52:51 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.67 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int n, int[] numlist) {
+        List<int> answer = new List<int>();
+        foreach(int i in numlist)
+        {
+            if(i % n == 0)
+            {
+                answer.Add(i);
+            }
+        }
+        return answer.ToArray();
+    }
+}
