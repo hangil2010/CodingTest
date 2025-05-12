b5d91822ea1cfe519f3bfdef1b93100a9973e509
Hangil <105782314+hangil2010@users.noreply.github.com>
2025-05-12 22:37:49 +0900

    [level 0] Title: 특이한 정렬, Time: 3.60 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+public class Solution {
+    public int[] solution(int[] numlist, int n) {
+        return numlist
+            .OrderBy(x => Math.Abs(x - n))
+            .ThenByDescending(x => x)      
+            .ToArray();
+    }
+}
