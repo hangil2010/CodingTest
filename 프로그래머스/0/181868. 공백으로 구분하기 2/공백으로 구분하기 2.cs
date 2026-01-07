bf105508148da205f5ad820215b13d5896297b75
Calmber <105782314+hangil2010@users.noreply.github.com>
2026-01-07 14:41:56 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 2.04 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Linq;
+public class Solution {
+    public string[] solution(string my_string) {
+        return my_string.Split(' ').Where(x => x != "").Select(x => x).ToArray();
+    }
+}
