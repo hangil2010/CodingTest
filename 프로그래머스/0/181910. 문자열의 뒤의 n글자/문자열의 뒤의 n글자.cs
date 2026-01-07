1f6eb70f4e3cfbfd4638d547dbe0b2ea2e39e5b0
Calmber <105782314+hangil2010@users.noreply.github.com>
2026-01-07 11:48:01 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.17 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int n) {
+        string answer = "";
+        
+        int startIndex = my_string.Length - n;
+        
+        return answer= my_string.Substring(startIndex);
+    }
+}
