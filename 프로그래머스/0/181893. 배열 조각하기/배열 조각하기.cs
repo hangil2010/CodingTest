095d669afab093ead7705a3da77c64586cda5c29
Calmber <105782314+hangil2010@users.noreply.github.com>
2026-01-12 21:27:46 +0900

    [level 0] Title: 배열 조각하기, Time: 1.87 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        List<int> arrList = arr.ToList();
+        List<int> tempList = new List<int>();
+        
+        for(int i = 0 ; i < query.Length ; i++)
+        {
+            
+            if(i % 2 == 0)
+            {
+                arrList = arrList.Take(query[i] + 1).ToList();
+            }
+            else
+            {
+                arrList = arrList.Skip(query[i]).ToList();
+            }
+            
+        }
+        
+        
+        return arrList.ToArray();
+    }
+}
